610a1ebd6f2f6a9fc5d9868398f1d7ead113c14d
CHOI DAE GEON <cdg@aplayz.co.kr>
2023-09-02 11:27:39 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 import './style.css'
+import Icon from './assets/img/facebook.png'
 function component() {
     const element = document.createElement('div');
 
     // 이 라인이 동작하려면 현재 스크립트를 통해 포함된 Lodash가 필요합니다.
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon)
+
 
     return element;
 }
